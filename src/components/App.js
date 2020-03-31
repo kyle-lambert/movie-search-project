a7f2c3eaeabc9324b1fecf91bec08f0305f8cd9c
kylelambert001 <kylelambert001@gmail.com>
2020-03-31 15:05:07 +1030

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from "react";
-import Home from "./pages/Home";
 import Movie from "./pages/Movie";
-import TV from "./pages/TV";
-import { Switch, Route } from "react-router-dom";
 import "../css/App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route
-            exact
-            path="/movie/:id"
-            render={routeProps => <Movie routeProps={routeProps} />}
-          />
-          <Route
-            exact
-            path="/tv/:id"
-            render={routeProps => <TV routeProps={routeProps} />}
-          />
-        </Switch>
+        <Movie />
       </div>
     );
   }
